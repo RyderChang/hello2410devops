13a3b26ba7f4504c6c4e4cfa938d9d8be5298010
RyderChang <zzryyy1202@outlook.com>
2024-10-12 14:56:58 +0800

    hello jenkins

---
+++ hello01demo/src/main/java/edu/hubu/hello01demo/Hello01demoApplication.java
@@ -0,0 +1,13 @@
+package edu.hubu.hello01demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hello01demoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hello01demoApplication.class, args);
+    }
+
+}
---
+++ hello01demo/src/test/java/edu/hubu/hello01demo/Hello01demoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hubu.hello01demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hello01demoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
